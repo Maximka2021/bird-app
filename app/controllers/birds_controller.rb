db1c44c1e6eb69c548a624b42f5b60c0526ea065
Maxim <polyakovmaxim05@gmail.com>
2023-01-02 13:50:16 -0500

    Show method

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,4 +4,11 @@ class BirdsController < ApplicationController
         render json: Bird.all
     end
 
+    def show
+        bird = Bird.find(params[:id])
+        render json: bird
+      rescue ActiveRecord::RecordNotFound
+        render json: "Bird not found", status: :not_found
+      end
+
 end
